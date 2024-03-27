f086a16cad1b5f3b298f5046759418b003ed2943
rod7773 <rodrigoarcadio1@gmail.com>
2024-03-27 09:54:28 +0100

    update

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,3 +1,11 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+
+const MY_AUTH_APP = "MY_AUTH_APP";
 
 export const AuthContext = createContext();
+
+export function AuthContextrovider({ children }) {
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    localStorage.getItem(MY_AUTH_APP)
+  );
+}
