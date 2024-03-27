2f67a4bcd52fa4a503e88dd8fbafe3486b55bf77
rod7773 <rodrigoarcadio1@gmail.com>
2024-03-27 11:34:00 +0100

    update

--- src/views/Private.js
+++ src/views/Private.js
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+import { LOGOUT } from "config/routes/path";
+
 function Private() {
-  return <h1>Private</h1>;
+  return (
+    <div>
+      <h1>Private</h1>
+      <Link to={LOGOUT}>Cerrar Sesion</Link>
+    </div>
+  );
 }
 
 export default Private;
